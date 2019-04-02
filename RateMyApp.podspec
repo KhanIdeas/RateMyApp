8356ac2e0b97bfddaf2a5d8ed4fc48dd11fc0821
KhanIdeas <ishx.gevorgyan@gmail.com>
2019-04-03 02:59:10 +0400

    corrections

--- RateMyApp.podspec
+++ RateMyApp.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'RateMyApp'
-  s.version          = '0.1.3'
+  s.version          = '0.1.4'
   s.summary          = 'Rate asker'
   s.swift_version    = '4.0'
 # This description is used to generate tags and improve search results.
--- UIViewController+Extension.swift
+++ UIViewController+Extension.swift
@@ -1,43 +1,43 @@
-
-
-import UIKit
-import StoreKit
-
-public extension UIViewController{
-   
-    public func addRateAskerCounter()
-    {
-        if UserDefaults.standard.value(forKey: "countOfRateAsks") == nil
-        {
-            UserDefaults.standard.setValue(1, forKey: "countOfRateAsks")
-            UserDefaults.standard.synchronize()
-        }
-        else
-        {
-            let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int) + 1
-            UserDefaults.standard.setValue(count, forKey: "countOfRateAsks")
-            UserDefaults.standard.synchronize()
-            
-        }
-        
-    }
-    public func runRateAskerCounter()
-    {
-        let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int)
-        if (count == 5 || count == 25 || count == 50)
-        {
-            
-            if #available( iOS 10.3,*){
-                SKStoreReviewController.requestReview()
-            }
-        }
-        else
-        {
-            let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int) - 1
-            UserDefaults.standard.setValue(count, forKey: "countOfRateAsks")
-            UserDefaults.standard.synchronize()
-        }
-        
-    }
-}
-
+//
+//
+//import UIKit
+//import StoreKit
+//
+//public extension UIViewController{
+//   
+//    public func addRateAskerCounter()
+//    {
+//        if UserDefaults.standard.value(forKey: "countOfRateAsks") == nil
+//        {
+//            UserDefaults.standard.setValue(1, forKey: "countOfRateAsks")
+//            UserDefaults.standard.synchronize()
+//        }
+//        else
+//        {
+//            let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int) + 1
+//            UserDefaults.standard.setValue(count, forKey: "countOfRateAsks")
+//            UserDefaults.standard.synchronize()
+//            
+//        }
+//        
+//    }
+//    public func runRateAskerCounter()
+//    {
+//        let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int)
+//        if (count == 5 || count == 25 || count == 50)
+//        {
+//            
+//            if #available( iOS 10.3,*){
+//                SKStoreReviewController.requestReview()
+//            }
+//        }
+//        else
+//        {
+//            let count = (UserDefaults.standard.value(forKey: "countOfRateAsks") as! Int) - 1
+//            UserDefaults.standard.setValue(count, forKey: "countOfRateAsks")
+//            UserDefaults.standard.synchronize()
+//        }
+//        
+//    }
+//}
+//
